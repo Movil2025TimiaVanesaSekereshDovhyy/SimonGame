f7d57684827d61a4601ee20831bd6f60af8d251c
a <a>
2025-12-02 19:40:09 +0100

    v1

--- app/src/main/java/net/iessochoa/sergiocontreras/simongame/MainActivity.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/simongame/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import net.iessochoa.sergiocontreras.simongame.ui.SimonGameApp
 import net.iessochoa.sergiocontreras.simongame.ui.theme.SimonGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimonGameTheme {
-                SimonGameApp
+                SimonGameApp()
 
             }
         }
