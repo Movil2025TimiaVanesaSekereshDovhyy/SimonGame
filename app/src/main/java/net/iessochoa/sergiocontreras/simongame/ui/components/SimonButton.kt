7bf53045e56fba76b0df4336c08f52dca8b4a535
a <a>
2025-12-02 20:12:24 +0100

    v2

--- app/src/main/java/net/iessochoa/sergiocontreras/simongame/ui/components/SimonButton.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/simongame/ui/components/SimonButton.kt
@@ -1,6 +1,12 @@
 package net.iessochoa.sergiocontreras.simongame.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 /**
  * Project: SimonGame
@@ -11,12 +17,25 @@ import androidx.compose.runtime.Composable
  */
 
 @Composable
-fun SimonButton() {
-
+fun SimonButton(
+    color: Color,
+    isActive: Boolean = false,
+    onClick: ()-> Unit,
+    modifier: Modifier = Modifier
+) {
     /* Representa a cada uno de los 4 botones que tiene el juego de Simon.
     A veces nos toca rascarnos la cabeza pensando en que propiedades necesitará...
     Una manera es tener el componente NO reutilizable e ir moviendo progresivamente parámetros a la firma.
     Como vamos a necesitar un Toast... ¿Qué información necesitará transmitir? Eso es una pista de la firma del lambda.
     */
+    //Si se pulsa se pone blanco
+    val botonActivo = if (isActive) Color.White else color
+
+    Box(
+        modifier = modifier
+            .aspectRatio(1f) //Cuadrado
+            .background(botonActivo)
+            .clickable { onClick}
+    )
 
 }
--- app/src/main/java/net/iessochoa/sergiocontreras/simongame/ui/screens/SimonGameScreen.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/simongame/ui/screens/SimonGameScreen.kt
@@ -1,9 +1,18 @@
 package net.iessochoa.sergiocontreras.simongame.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import net.iessochoa.sergiocontreras.simongame.model.SimonColor
+import net.iessochoa.sergiocontreras.simongame.ui.components.SimonButton
 
 /**
  * Project: SimonGame
@@ -17,12 +26,60 @@ import androidx.compose.ui.platform.LocalContext
 fun SimonGameScreen(
     modifier: Modifier = Modifier
 ) {
+    /* ENUNCIADO 2: Completa el tablero de juego. Es un grid 2x2 con los colores de Simon
+  [Verde, Rojo]
+  [Amarillo, Azul]
+  De momento que ocupe toda la pantalla
+  Los deberás instanciar utilizando los SimonButton
+ */
+    //Para que me deje mostrar un toast, abrir un activity... (Devuelve el contexto actual)
+    val context = LocalContext.current
 
-  /* ENUNCIADO 2: Completa el tablero de juego. Es un grid 2x2 con los colores de Simon
-    [Verde, Rojo]
-    [Amarillo, Azul]
-    De momento que ocupe toda la pantalla
-    Los deberás instanciar utilizando los SimonButton
-   */
+    //Qué botón está activo
+    var colorActivo by remember { mutableStateOf<SimonColor?>(null) }
 
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row (modifier = Modifier.weight(1f)){
+            SimonButton(
+                SimonColor.Green.value,
+                colorActivo == SimonColor.Green, //Boolean para ver si se ha "activado"
+                {
+                    colorActivo = SimonColor.Green //Indicamos que está activo
+                    Toast.makeText(context, SimonColor.Green.label, Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.weight(1f)
+            )
+            SimonButton(
+                SimonColor.Red.value,
+                colorActivo == SimonColor.Red, //Boolean para ver si se ha "activado"
+                {
+                    colorActivo = SimonColor.Red //Indicamos que está activo
+                    Toast.makeText(context, SimonColor.Red.label, Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row (modifier = Modifier.weight(1f)) {
+            SimonButton(
+                SimonColor.Yellow.value,
+                colorActivo == SimonColor.Yellow, //Boolean para ver si se ha "activado"
+                {
+                    colorActivo = SimonColor.Yellow //Indicamos que está activo
+                    Toast.makeText(context, SimonColor.Yellow.label, Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.weight(1f)
+            )
+            SimonButton(
+                SimonColor.Blue.value,
+                colorActivo == SimonColor.Blue, //Boolean para ver si se ha "activado"
+                {
+                    colorActivo = SimonColor.Blue //Indicamos que está activo
+                    Toast.makeText(context, SimonColor.Blue.label, Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
